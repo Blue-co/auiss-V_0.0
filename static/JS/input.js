74d6601fadc78c388cbcaab5ca16ac2951b7997f
Blue-co <aa39017867@gmail.com>
2026-06-04 08:38:33 +0900

    Fix typo in textarea querySelector

--- static/JS/input.js
+++ static/JS/input.js
@@ -26,7 +26,7 @@ function brE() {
   // event.preventDefault()가 필요할 수 있습니다.
 }
 
-const textarea = document.querySelector('textarea");
+const textarea = document.querySelector('textarea');
 
 textarea.addEventListener('input', function() {
   // 높이를 초기화한 후, 내부 컨텐츠 크기(scrollHeight)만큼 높이를 키웁니다.
